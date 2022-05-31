d3b62e7bac530eb11941af5eec4f247962ff99a3
NanasoDev <mariano.ceballos@gmail.com>
2022-05-31 18:34:08 -0300

    fix: No More Status Magic Strings

--- src/modules/role/role.entity.ts
+++ src/modules/role/role.entity.ts
@@ -1,4 +1,5 @@
 
+import { status } from 'src/shared/entity-status.enum';
 import { BaseEntity, Column, Entity, JoinColumn, ManyToMany, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { User } from '../user/user.entity';
 
@@ -17,7 +18,7 @@ export class Role extends BaseEntity {
     @JoinColumn()
     users: User[]
 
-    @Column( { type: 'varchar', default: 'ACTIVE', length: 8 } )
+    @Column( { type: 'varchar', default: status.ACTIVE, length: 8 } )
     status: string;
 
     @CreateDateColumn( { type: 'timestamp', name: 'created_at' } )
